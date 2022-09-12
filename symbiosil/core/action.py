5c9e53482afe9fb1fbe68c5078f45b62e6f2765f
Ashish <spiraldeveloper@gmail.com>
2022-09-12 16:25:10 +0530

    * action minor update * sense processor added

--- symbiosil/core/action.py
+++ symbiosil/core/action.py
@@ -1,5 +1,13 @@
 import abc
+from enum import Enum
+
+
+class ActionType(Enum):
+    VOICE = 'voice'
+    WRITE = 'write'
 
 
 class Action(abc.ABC):
-    pass
+
+    def __init__(self, action_type: ActionType, **kwargs):
+        self.action_type = action_type
