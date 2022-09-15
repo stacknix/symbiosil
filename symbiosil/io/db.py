d877ab7296c31d96d621b4e5be20e01d8a11ab58
Ashish <spiraldeveloper@gmail.com>
2022-09-15 22:28:33 +0530

    MongoDBStore added for profiling

--- symbiosil/io/db.py
+++ symbiosil/io/db.py
@@ -1,7 +1,14 @@
 import redis
+import pymongo
 
 
 class RedisStore:
 
     def __init__(self, host, port, db, **kwargs):
         self.store = redis.Redis(host=host, port=port, db=db)
+
+
+class MongoDBStore:
+
+    def __init__(self, host, port, db, **kwargs):
+        self.store = pymongo.MongoClient(host, port)
